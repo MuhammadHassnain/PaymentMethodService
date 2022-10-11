c1d2235e8972d9cb0e845e1852350dbfdb4947df
Muhammad Hassnain <muhammadhassnain776@gmail.com>
2022-10-11 20:30:52 +0500

    Initial Commit

---
+++ src/main/java/com/starzplay/paymentmethodservice/PaymentMethodServiceApplication.java
@@ -0,0 +1,13 @@
+package com.starzplay.paymentmethodservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaymentMethodServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PaymentMethodServiceApplication.class, args);
+	}
+
+}
